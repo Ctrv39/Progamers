20053962e01106befefde20afd7417dfcee4c8e0
Viktor Lee <viktor.lee@jublia.com>
2021-12-07 22:48:03 +0700

    Fix unit test

--- ProgamersExpert/ProgamersExpertTests/ProgamersExpertTests.swift
+++ ProgamersExpert/ProgamersExpertTests/ProgamersExpertTests.swift
@@ -2,7 +2,7 @@
 //  ProgamersExpertTests.swift
 //  ProgamersExpertTests
 //
-//  Created by Viktor . on 31/10/21.
+//  Created by Victor . on 07/12/21.
 //
 
 import XCTest
@@ -10,35 +10,35 @@ import RxSwift
 @testable import ProgamersExpert
 
 class ProgamersExpertTests: XCTestCase {
-    private let disposeBag = DisposeBag()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-    
+
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
 
     func testHomeInteractor() throws {
+        let disposeBag = DisposeBag()
         // given
         let homeUseCase = Injection.init().provideHome()
-        
+
         // when
         homeUseCase.getGameList(page: 0).observe(on: MainScheduler.instance)
-            .subscribe{ _ in
+            .subscribe { _ in
                 // then
                 XCTAssert(homeUseCase.isGetGameCalled)
             }onError: { _ in
@@ -48,4 +48,5 @@ class ProgamersExpertTests: XCTestCase {
             }
             .disposed(by: disposeBag)
     }
+
 }
