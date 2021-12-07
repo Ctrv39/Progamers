013e7a632e5c08bbba75cae89493c25fc2cb4c44
Viktor Lee <viktor.lee@jublia.com>
2021-12-07 23:53:32 +0700

    Fix pod install

---
+++ ProgamersExpert/Pods/Target Support Files/Pods-ProgamersExpertUITests/Pods-ProgamersExpertUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProgamersExpertUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ProgamersExpertUITests
+@end
--- ProgamersExpert/Pods/Target Support Files/Pods-ProgamersExpertUITests/Pods-ProgamersExpertUITests-umbrella.h
+++ ProgamersExpert/Pods/Target Support Files/Pods-ProgamersExpertUITests/Pods-ProgamersExpertUITests-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_ProgamersExpert_ProgamersExpertUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ProgamersExpert_ProgamersExpertUITestsVersionString[];
+FOUNDATION_EXPORT double Pods_ProgamersExpertUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ProgamersExpertUITestsVersionString[];
 
